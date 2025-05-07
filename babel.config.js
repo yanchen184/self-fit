4c9fb7d25b1d57027caa8f680210033cc94b2b84
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-05-07 17:09:18 +0800

    移除nativewind相關配置

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['nativewind/babel', { mode: 'transformOnly' }],
       'react-native-reanimated/plugin'
     ],
   };
