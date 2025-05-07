a24cc8cd2c3c57ef659cce2ce5d4a07927155e56
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-05-07 17:08:36 +0800

    更新babel配置修復NativeWind問題

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'nativewind/babel',
+      ['nativewind/babel', { mode: 'transformOnly' }],
       'react-native-reanimated/plugin'
     ],
   };
